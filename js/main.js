9e7733120badd473f259bcb2b130d3a304781d41
travis@thirdstream.ca <travis@thirdstream.ca>
2021-09-01 23:52:14 -0400

    First Commit - Day 1 of building online CV

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    const client = new XMLHttpRequest();
+    client.open('GET','/codefile.txt');
+    client.onreadystatechange = function (){
+        let codeArea = $('#code-area');
+        if(codeArea.html() !== '')
+        {
+            return;
+        }
+        codeArea.append(client.responseText);
+        hljs.highlightElement(codeArea[0]);
+    }
+    client.send();
+
+});
